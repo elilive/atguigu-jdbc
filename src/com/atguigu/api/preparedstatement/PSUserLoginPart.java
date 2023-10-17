11003f57013a42f82d9efd659c8708a01e54ce26
Eli <1163730440@qq.com>
2023-10-17 16:38:40 +0800

    commit3

---
+++ src/com/atguigu/api/preparedstatement/PSUserLoginPart.java
@@ -0,0 +1,47 @@
+package com.atguigu.api.preparedstatement;
+
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ * ClassName: PSUserLoginPart
+ * Package: com.atguigu.api.preparedstatement
+ * Description:
+ *
+ * @Author Eli-Chen
+ * @Create 2023/10/17 15:29
+ * @Version 1.0
+ */
+public class PSUserLoginPart {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        //1.键盘输入事件，收集账号和密码信息
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入账号");
+        String account = scanner.nextLine();
+
+        System.out.println("请输入密码");
+        String password = scanner.nextLine();
+        //2.注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 3.获取连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/atguigu", "root", "root");
+        //4.创建PreparedStatement
+        String sql = "select * from t_user where account = ? and password = ? ;";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setObject(1, account);
+        preparedStatement.setObject(2, password);
+        //5.发送查询SQL语句，并获取返回结果
+        ResultSet resultSet = preparedStatement.executeQuery();
+        //6.结果判断，显示登录成功还是失败
+        if (resultSet.next()){
+            System.out.println("登陆成功");
+        }else {
+            System.out.println("登录失败");
+        }
+        //7.关闭资源
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        scanner.close();
+    }
+}
