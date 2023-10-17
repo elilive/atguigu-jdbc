c8b065c27a5a428319f292673cfd3be4a9c80382
Eli <1163730440@qq.com>
2023-10-18 00:04:07 +0800

    commit8

--- src/com/atguigu/api/utils/JdbcUtils.java
+++ src/com/atguigu/api/utils/JdbcUtils.java
@@ -1,5 +1,15 @@
 package com.atguigu.api.utils;
 
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.alibaba.druid.pool.ha.selector.DataSourceSelectorFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  * ClassName: JdbcUtils
  * Package: com.atguigu.api.utils
@@ -10,4 +20,23 @@ package com.atguigu.api.utils;
  * @Version 1.0
  */
 public class JdbcUtils {
+    private static DataSource dataSource = null;
+    static {
+        Properties properties = new Properties();
+        InputStream inputStream = JdbcUtils.class.getClassLoader().getResourceAsStream("druid.properties");
+        try {
+            properties.load(inputStream);
+            dataSource = DruidDataSourceFactory.createDataSource(properties);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+    public static void closeConnection(Connection connection) throws SQLException {
+        connection.close();
+    }
 }
